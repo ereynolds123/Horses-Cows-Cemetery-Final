1497db26e6d1f3a68b5988e3ad1a8f704864acf5
Emily Reynolds <ereynold@estee.com>
2023-07-10 11:56:34 -0400

    Local Storage somewhat working

--- Horses, Cows, Cemeteries Final/public/script.js
+++ Horses, Cows, Cemeteries Final/public/script.js
@@ -1,42 +1,65 @@
-// Observational game that lets you count how many horses, cows and cemeteries you see
-
 //Declaring Variables
 let scoreText;
 let scoreTeamOne = 0;
 let scoreTeamTwo = 0;
 
+// Storing score in local storage to persist the data
+
+localStorage.setItem("scoreTeamOne", scoreTeamOne);
+localStorage.setItem("scoreTeamTwo", scoreTeamTwo);
+
+// Retrieving score from local storage when the page loads
+
+window.addEventListener("load", function() {
+    scoreTeamOne = parseInt(localStorage.getItem("scoreTeamOne"));
+    scoreTeamTwo = parseInt(localStorage.getItem("scoreTeamTwo"));
+    document.getElementById("scoreTotalTeamOne").innerHTML = scoreTeamOne;
+    document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
+});
+
+// Updating score in local storage when it changes
+
+function updateLocalStorage() {
+    localStorage.setItem("scoreTeamOne", scoreTeamOne);
+    localStorage.setItem("scoreTeamTwo", scoreTeamTwo);
+}
 
 //Adding to Team One Score
 function addTenTeamOne() {
     scoreTeamOne = scoreTeamOne + 10;
     document.getElementById("scoreTotalTeamOne").innerHTML = scoreTeamOne;
+    updateLocalStorage();
 };
 
 function subtractOneTeamOne() {
     scoreTeamOne = scoreTeamOne - 1;
     document.getElementById("scoreTotalTeamOne").innerHTML = scoreTeamOne;
+    updateLocalStorage();
 };
 
 function subtractOneHundredTeamOne() {
     scoreTeamOne = scoreTeamOne - 100;
     document.getElementById("scoreTotalTeamOne").innerHTML = scoreTeamOne;
+    updateLocalStorage();
 };
-
 
 //Adding to Team Two Score
 function addTenTeamTwo() {
     scoreTeamTwo = scoreTeamTwo + 10;
     document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
+    updateLocalStorage();
 };
 
 function subtractOneTeamTwo() {
     scoreTeamTwo = scoreTeamTwo - 1;
     document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
+    updateLocalStorage();
 };
 
 function subtractOneHundredTeamTwo() {
     scoreTeamTwo = scoreTeamTwo - 100;
     document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
+    updateLocalStorage();
 };
 
 //Alert Winner and Restart the game
@@ -57,13 +80,12 @@ function winningMessage() {
     scoreTeamTwo = 0;
     document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
 
+    updateLocalStorage();
 }
 
 function instructionsMessage() {
     alert("If you spot a horse on your team's side your team gets 10 points.\n\nIf you spot a cow on your opponent's side your opponent loses 1 point.\n\nIf you spot a cemetery on your opponent's side your opponent loses 100 points.\n\nWhen you spot something, click on the icon associated with the side it's on.\n\nYour game will save in the browser!")
 }
-
-
 
 //This section clicks on the various buttons for Team One and adds to the point total Team One. 
 let clickOnTeamOneButtons =
