416309cd99143377b837f81af2a0d448a0ad0075
Emily Reynolds <ereynold@estee.com>
2023-07-10 19:36:26 -0400

    All functionality working:

--- Horses, Cows, Cemeteries Final/public/script.js
+++ Horses, Cows, Cemeteries Final/public/script.js
@@ -13,11 +13,11 @@ if(scoreTeamTwoTemp){
     scoreTeamTwo = parseInt(scoreTeamTwoTemp)
  }
 
- console.log(`scores: ${scoreTeamOne} and ${scoreTeamTwo}`)
+
 // Storing score in local storage to persist the data
 localStorage.setItem("scoreTeamOne", scoreTeamOne);
 localStorage.setItem("scoreTeamTwo", scoreTeamTwo);
-console.log(`scores: ${scoreTeamOne} and ${scoreTeamTwo}`)
+
 
 document.getElementById("scoreTotalTeamOne").innerHTML = scoreTeamOne;
 document.getElementById("scoreTotalTeamTwo").innerHTML = scoreTeamTwo;
@@ -96,80 +96,27 @@ function instructionsMessage() {
 
 //This section clicks on the various buttons for Team One and adds to the point total Team One. 
 let clickOnTeamOneButtons =
-    document.getElementById('horseTeamOne').addEventListener("click", addTenTeamOne);
+ document.getElementById('horseTeamOne').addEventListener("click", addTenTeamOne);
 
 document.getElementById('cowTeamOne').addEventListener("click", subtractOneTeamOne);
 
 document.getElementById('cemeteryTeamOne').addEventListener("click", subtractOneHundredTeamOne);
 
 //This section clicks on the various buttons for Team Two and adds to the point total for Team Two. 
-let clickOnTeamTwoButtons =
-    document.getElementById('horseTeamTwo').addEventListener("click", addTenTeamTwo);
-
+let clickOnTeamTwoButtons = document.getElementById('horseTeamTwo').addEventListener("click", addTenTeamTwo);
 document.getElementById('cowTeamTwo').addEventListener("click", subtractOneTeamTwo);
-
 document.getElementById('cemeteryTeamTwo').addEventListener("click", subtractOneHundredTeamTwo);
-
-function instructionsMessage() {
-    // Create popup div
-    var popupDiv = document.createElement('div');
-    popupDiv.id = 'popup';
-    
-    // Create popup content
-    var popupContent = document.createElement('div');
-    popupContent.id = 'popup-content';
-    
-    // Create title
-    var title = document.createElement('h2');
-    title.textContent = 'Instructions';
-    popupContent.appendChild(title);
-    
-    // Create message
-    var message = document.createElement('p');
-    popupContent.appendChild(message);
-    
-    // Create list
-    var list = document.createElement('ul');
-    var listItem1 = document.createElement('li');
-    listItem1.textContent = 'Click on the horse button to when you see a horse on your side of the car';
-    list.appendChild(listItem1);
-    var listItem2 = document.createElement('li');
-    listItem2.textContent = 'When you opponent spots a cow on your side, hit the cow button';
-    list.appendChild(listItem2);
-    var listItem3 = document.createElement('li');
-    listItem3.textContent = 'When you cemetery spots a cow on your side, hit the cemetery button';
-    list.appendChild(listItem3);
-    popupContent.appendChild(list);
-    var listItem4 = document.createElement('li');
-    listItem4.textContent = 'Drive safely! This scorekeeper is for passengers only.';
-    list.appendChild(listItem4);
-    popupContent.appendChild(list);
-    
-    // Create close button
-    var closeButton = document.createElement('button');
-    closeButton.textContent = 'Close';
-    closeButton.addEventListener('click', function() {
-      popupDiv.remove();
-    });
-    popupContent.appendChild(closeButton);
-    
-    // Append popup content to popup div
-    popupDiv.appendChild(popupContent);
-    
-    // Append popup div to body
-    document.body.appendChild(popupDiv);
-  }
 
 
 
   function winningMessage() {
     var result;
     if (scoreTeamOne > scoreTeamTwo) {
-        result = "Team One has won!";
+        result = `Team One has won with a score of ${scoreTeamOne}`;
     } else if (scoreTeamTwo > scoreTeamOne) {
-        result = "Team Two has won!";
+        result = `Team Two has won with a score of ${scoreTeamTwo}`;
     } else {
-        result = "It's a tie!";
+        result = `It's a tie with a score of ${scoreTeamOne}`;
     }
 
     // Create popup div
@@ -192,9 +139,9 @@ function instructionsMessage() {
 
     // Create close button
     var closeButton = document.createElement('button');
-    closeButton.textContent = 'Close';
-    closeButton.addEventListener('click', function() {
-        popupDiv.remove();
+    closeButton.textContent = 'Start a new game';
+    closeButton.addEventListener('click', function(){
+        resetValues()
     });
     popupContent.appendChild(closeButton);
 
@@ -203,5 +150,16 @@ function instructionsMessage() {
 
     // Append popup div to body
     document.body.appendChild(popupDiv);
+
+    function resetValues(){
+        popupDiv.remove(); 
+        localStorage.setItem("scoreTeamOne", "0");
+        localStorage.setItem("scoreTeamTwo", "0");
+        location.reload();
+    }
+
 }
+
+
+
 
